b4656fbe693cefae47668adb4c411bfe939b43ce
Joel Azemar <joel.azemar@gmail.com>
2023-08-11 16:57:11 +0200

    Initial Commit

--- lib/ar_ulid/generators/ulid.rb
+++ lib/ar_ulid/generators/ulid.rb
@@ -10,6 +10,7 @@ module ArUlid
         def generate_id
           time = ArUlid.configuration.time_method.call
           an_event_identifier = ArUlid.configuration.an_event_identifier.call
+
           ULID.generate(time, suffix: an_event_identifier)
         end
       end
